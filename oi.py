a30d9468227194603dad238ce603dad9cb6f8fba
LeandroCaceresM <le.caceres@duocuc.cl>
2026-05-30 01:50:48 -0400

    Primer commit
    Co-authored-by: NicolasLizama <n.lizama@duocuc.cl>

--- oi.py
+++ oi.py
@@ -1,3 +1,4 @@
 while True:
   print("No debo usar ChatGpt para hacer trampa en mis tareas DOU")
   print("Si lo hago, tendré problemas con mis profesores y no aprenderé nada")
+  print("Es importante que haga mis tareas por mi cuenta para aprender y crecer como estudiante")
